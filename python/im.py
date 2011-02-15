3b677a0fd1accdccb53f3da0e855e037764c286e
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2011-02-15 17:03:10 -0500

    [what] * Add Font and merge (the inverse of split)

--- python/im.py
+++ python/im.py
@@ -16,6 +16,15 @@ color.RED=(0,0,255,0)
 color.GREEN=(0,255,0,0)
 color.BLUE=(255,0,0,0)
 
+class Font(object):
+    pass
+
+font = Font()
+font.default = cv.InitFont(cv.CV_FONT_HERSHEY_DUPLEX, 1.0, 1.0, thickness=2)
+
+def puttext(img, text, x, y):
+  cv.PutText(img, text, (x,y), font.default, color.RED)
+
 def cvimg2numpy(cvimg):
   return numpy.asarray(cv.GetMat(cvimg))
   
@@ -59,6 +68,12 @@ def split3(cvimg):
   c3 = cv.CreateImage(size, cv.IPL_DEPTH_8U, 1)
   cv.Split(cvimg, c1, c2, c3, None)
   return c1,c2,c3
+
+def merge3(b,g,r):
+  size = (r.width, r.height)
+  img = cv.CreateImage(size, cv.IPL_DEPTH_8U, 3)
+  cv.Merge(b,g,r,None,img)
+  return img
   
 def size(cvimg):
   return (cvimg.width, cvimg.height)
@@ -123,25 +138,6 @@ def find_convex_defects(contour, hull):
     return cv.ConvexityDefects(contour, hull, storage)
 
 
-def find_max_rectangle(contours):
-    lefts, tops = [], []
-    max_right, max_bottom = 0, 0
-    try:
-      while True:
-          left, top, w, h = cv.BoundingRect(contours)
-          right = left + w
-          bottom = top + h
-          lefts.append(left)
-          tops.append(top)
-          if right > max_right:
-              max_right = right
-          if bottom > max_bottom:
-              max_bottom = bottom
-          contours = contours.h_next()
-    except TypeError, e:
-      return min(lefts), min(tops), max_right, max_bottom
-    return min(lefts), min(tops), max_right, max_bottom
-
 def max_area(contours):
     ''' returns the contour with maximal area. 
         @return: (max_area, max_contour)
@@ -159,6 +155,13 @@ def max_area(contours):
       return max_area, max_contours
     return max_area, max_contours
 
+def find_max_rectangle(contours):
+    max_a, contours = max_area(contours)
+    left, top, w, h = cv.BoundingRect(contours)
+    right = left + w
+    bottom = top + h
+    return left, top, right, bottom
+
 def plot_contours(contours, shape):
     img = cv.CreateImage(shape, 8, 3)
     cv.NamedWindow('show', 1)
@@ -168,8 +171,12 @@ def plot_contours(contours, shape):
 
 
 if __name__=='__main__':
-  im = cv.LoadImage('mask.png')
-  im = bgr2gray(im)
+  im = cv.LoadImage('orig.png')
+  #im = bgr2gray(im)
+  b,g,r = split3(im)
+  im2 = merge3(b,g,r)
+  cv.SaveImage('out.png', im2)
+  exit(0)
   contours = find_contours(im)
   area = cv.ContourArea(contours)
   print area
